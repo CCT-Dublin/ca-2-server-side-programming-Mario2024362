2da2bd78f2c8b3a653f3d6056673a8b0f866a491
Mario2024362 <2024362@student.cct.ie>
2025-12-16 23:38:08 +0000

    this is the simple database sql that was created in workbench

---
+++ server/personal-information.sql
@@ -0,0 +1,8 @@
+CREATE TABLE personal_table (
+  id INT AUTO_INCREMENT PRIMARY KEY,
+  first_name VARCHAR(20),
+  second_name VARCHAR(20),
+  email VARCHAR(50),
+  phone_number VARCHAR(10),
+  eircode VARCHAR(6)
+);
