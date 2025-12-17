535f094fd3870a7b86270d03ae894d65e9e0b2e1
Mario2024362 <2024362@student.cct.ie>
2025-12-17 23:32:04 +0000

    Connection to the database was created

---
+++ server/Database.js
@@ -0,0 +1,12 @@
+// created the import and the connection of the database
+const mysql= require("mysql12");
+
+//Make the connection with the database
+const data=mysql.createconnection({
+//Information  to identified the database that want to be connect
+     host:"localhost",  
+    user:"root",
+    password:"",
+    database: "Personal_information(1)",
+
+});
