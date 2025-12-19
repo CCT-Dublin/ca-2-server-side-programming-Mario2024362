409642b9c83b8a52c0f1579e68f7660f86b9140e
Mario2024362 <2024362@student.cct.ie>
2025-12-19 09:43:07 +0000

    database in sql was updated

--- server/personal-information.sql
+++ server/personal-information.sql
@@ -1,8 +1,16 @@
-CREATE TABLE personal_table (
+-- Create the database
+CREATE DATABASE IF NOT EXISTS personal_table;
+
+-- Use the database
+USE personal_table;
+
+-- Create the table
+CREATE TABLE IF NOT EXISTS mysql_table (
   id INT AUTO_INCREMENT PRIMARY KEY,
-  first_name VARCHAR(20),
-  second_name VARCHAR(20),
-  email VARCHAR(50),
-  phone_number VARCHAR(10),
-  eircode VARCHAR(6)
+  first_name VARCHAR(20) NOT NULL,
+  second_name VARCHAR(20) NOT NULL,
+  email VARCHAR(80) NOT NULL,
+  phone_number VARCHAR(10) NOT NULL,
+  eircode VARCHAR(6) NOT NULL
 );
+SELECT * FROM mysql_table;
