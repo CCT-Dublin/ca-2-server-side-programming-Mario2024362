cfc3dddc3efc96bb987c25e16a6f0f8c96ecafae
Mario2024362 <2024362@student.cct.ie>
2025-12-21 01:01:22 +0000

    database was fixed

--- server/Database.js
+++ server/Database.js
@@ -10,17 +10,22 @@ const data=mysql.createconnection({
     database: "Personal_information(1)",
 
 });
-// a message will be print if the connection is workin or not 
-data.connect((err)=>{
-    //if there is any error with the database connection
-    if(err){
-console.error.apply("Error connection failed:" , err.message);
-return;
+function ensureSchema(callback) {
+  const sql = `
+    CREATE TABLE IF NOT EXISTS mysql_table (
+      id INT AUTO_INCREMENT PRIMARY KEY,
+      first_name VARCHAR(30),
+      second_name VARCHAR(30),
+      email VARCHAR(80),
+      phone_number VARCHAR(10),
+      eircode VARCHAR(6)
+    )
+  `;
+  db.query(sql, callback);
 }
-//if everything is working will print this
-console.log("Connection with database is working");
 
-})
+
+
 //export the database connection
 MediaSourceHandle.exports={
     data,
