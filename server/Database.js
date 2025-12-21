dd9a8d53adc22606e689832cd08c3c7c28642ed9
Mario2024362 <2024362@student.cct.ie>
2025-12-21 10:27:25 +0000

    Minor errors were fixed

--- server/Database.js
+++ server/Database.js
@@ -1,13 +1,13 @@
 // created the import and the connection of the database
-const mysql= require("mysql12");
+const mysql= require("mysql2");
 
 //Make the connection with the database
 const data=mysql.createconnection({
 //Information  to identified the database that want to be connect
      host:"localhost",  
     user:"root",
-    password:"",
-    database: "Personal_information(1)",
+    password:"362.Martinez",
+    database: "Personal_information",
 
 });
 function ensureSchema(callback) {
